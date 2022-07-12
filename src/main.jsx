d6e75c91646248e788ac195696a4da796129f692
Fausto Gutiérrez Tépoz <faustogute@MacBook-Air-M1.local>
2022-07-11 19:46:30 -0500

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// librerías
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// estilos
+import './styles/index.css'
+
+// componentes
+import App from './components/App'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
